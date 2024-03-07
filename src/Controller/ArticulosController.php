107a384e9c46d26c80a233e706cc944930e3c6f3
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2024-03-07 10:49:54 +0100

    initial commit

---
+++ src/Controller/ArticulosController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticulosController extends AbstractController
+{
+    private $articulos = [
+        [
+            'id' => 1,
+            'title' => 'Articulo 1',
+            'created' => '2021-10-01',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Articulo 2',
+            'created' => '2021-10-02',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Articulo 3',
+            'created' => '2021-10-03',
+        ],
+        [
+            'id' => 4,
+            'title' => 'Articulo 4',
+            'created' => '2021-10-04',
+        ],
+        [
+            'id' => 5,
+            'title' => 'Articulo 5',
+            'created' => '2021-10-05',
+        ]
+    ];
+
+    #[Route('/articulos', name: 'app_main')]
+    public function show_articulos(): Response
+    {
+        return $this->render('articulos.html.twig', [
+            'articulos' => $this->articulos
+        ]);
+    }
+
+
+    #[Route('/articulo/{id}', name: 'app_main_id')]
+    public function find_articulo($id = 2): Response
+    {
+        $articulo = array_filter($this->articulos, function ($a) use ($id) {
+            return $a['id'] == $id;
+        });
+
+        if ($id == 1) {
+            return $this->redirectToRoute('app_main_id_1');
+        } else {
+            return $this->render('articulo.html.twig', [
+                'articulo' => array_pop($articulo)
+            ]);
+        }
+    }
+
+    #[Route('/articulo1', name: 'app_main_id_1')]
+    public function find_articulo_1(): Response
+    {
+        $articulo = $this->articulos[0];
+
+        return $this->render('articulo1.html.twig', [
+            'articulo' => $articulo
+        ]);
+    }
+}
